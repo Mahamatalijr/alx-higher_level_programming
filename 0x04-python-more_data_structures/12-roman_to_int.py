33861de3b89c0bcf5f706c2ada3395fe0b60ea1f
Mahamat Ali Brahim Khalil <mahamatalibrahimkhalil4@gmail.com>
2023-01-13 10:32:21 +0000

    roman to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,11 +3,11 @@ def to_subtract(list_num):
     to_sub = 0
     max_list = max(list_num)
 
- for n in list_num:
+    for n in list_num:
         if max_list > n:
             to_sub += n
 
- return (max_list - to_sub)
+    return (max_list - to_sub)
 
 
 def roman_to_int(roman_string):
@@ -36,5 +36,5 @@ def roman_to_int(roman_string):
                 last_rom = rom_n.get(ch)
 
     num += to_subtract(list_num)
-    
+
     return (num)
