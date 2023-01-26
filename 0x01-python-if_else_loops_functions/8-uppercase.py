51192e6ed764c95e9b3694f8fcea28be635eeebe
Mahamat Ali Brahim Khalil <mahamatalibrahimkhalil4@gmail.com>
2023-01-26 12:31:46 +0000

    uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,8 @@
-#!/usr/bin/env python3
-uppercase = __import__('8-uppercase').uppercase
-
-uppercase("holberton")
-uppercase("Holberton Scholl 98 Battery street")
+#!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+        change = 0
+        if ord(i) > 96 and ord(i) < 123:
+            change = 32
+        print("{:s}".format(chr(ord(i) - change)), end="")
+    print()
