fdc67cddfdaf94077f54d8a4a64fa0a0ef37b26d
Mahamatalijr <100375102+Mahamatalijr@users.noreply.github.com>
2024-01-06 23:44:52 +0530

    Delete 0x01-python-if_else_loops_functions directory

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-def uppercase(str):
-    for i in str:
-        change = 0
-        if ord(i) > 96 and ord(i) < 123:
-            change = 32
-        print("{:s}".format(chr(ord(i) - change)), end="")
-    print()
